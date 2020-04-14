682a563a3cc9ae01993af0d5b6cc4fa5638c34a2
Daniel <lemebor91@yahoo.com.br>
2020-04-14 12:13:18 -0300

    adicionando compartilhamento de estado Vuex

---
+++ src/components/Resumo.vue
@@ -0,0 +1,53 @@
+<template>
+    <Painel titulo="Resumo" roxo>
+        <div class="resumo">
+            <span>Total: <strong>{{ total | dinheiro }}</strong></span>
+            <hr>
+            <button>Finalizar!</button>
+        </div>
+    </Painel>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex'
+export default {
+
+    
+    // data() {
+    //     return {
+    //         produtos: [
+    //             { nome: 'Produto 1', quantidade: 7, preco: 14.55 },
+    //             { nome: 'Produto 2', quantidade: 10, preco: 22.99 },
+    //             { nome: 'Produto 3', quantidade: 1, preco: 43.18 },
+    //         ]
+    //     }
+    // }
+
+
+    // computed: mapGetters(['valorTotal'])
+
+    computed: mapGetters('carrinho', {
+        total: 'valorTotal'
+    })
+        // total() {
+        //     return this.$store.getters.valorTotal
+        // },
+        // produtos() {
+        //     return this.$store.state.produtos
+        // }
+    
+
+}
+</script>
+
+<style>
+    table {
+        width: 100%;
+    }
+
+    td {
+        border-top: 1px solid #EEE;
+        width: 33%;
+    }
+</style>
